179d9342c7ed28cdf85861d11dfde971048ed2f7
f3liz <125415218+f3liz@users.noreply.github.com>
2024-11-15 10:37:35 -0800

    Add files via upload

---
+++ Go/07_loops/main.go
@@ -0,0 +1,36 @@
+// need for main
+package main
+
+// need to print
+import "fmt"
+
+// function
+func main() {
+	// Long method
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		// i = i + 1
+		i++
+	}
+
+	// Short method
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("Number: %d\n", i)
+	}
+
+	// FizzBuzz
+	for i := 1 ; i <= 100; i++ {
+		// i % 3 == 0 && i % 5 == 0 is same as i % 15 == 0
+		if i % 15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
